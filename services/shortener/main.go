3a72762a58bdf609fe5226808a4dc7bcf4524a3a
VHall1 <victorhallpsn@hotmail.com>
2025-01-22 16:57:34 +0000

    actually initialise shortener routes

--- services/shortener/main.go
+++ services/shortener/main.go
@@ -4,6 +4,9 @@ import (
 	"log"
 
 	"github.com/vhall1/shorturl/lib/bootstrap"
+	"github.com/vhall1/shorturl/services/shortener/handler"
+	"github.com/vhall1/shorturl/services/shortener/service"
+	"github.com/vhall1/shorturl/services/shortener/store"
 )
 
 func main() {
@@ -11,6 +14,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	psql, err := bootstrap.NewPostgres()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	store := store.NewUrlStore(psql)
+	svc := service.NewShortenerService(store)
+	h := handler.NewShortenerHttpHandler(svc)
+	h.RegisterRoutes(httpServer.Mux)
 
 	if err := httpServer.Start(); err != nil {
 		log.Fatal(err)
