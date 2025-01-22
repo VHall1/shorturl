468e19b0c36d33f9f09a04de93916840392a6c7e
VHall1 <victorhallpsn@hotmail.com>
2025-01-22 16:02:47 +0000

    remove .env loading

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/vhall1/shorturl
 
 go 1.23.4
 
-require (
-	github.com/joho/godotenv v1.5.1
-	github.com/vrischmann/envconfig v1.3.0
-)
+require github.com/vrischmann/envconfig v1.3.0
--- lib/config/config.go
+++ lib/config/config.go
@@ -2,23 +2,11 @@ package config
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/vrischmann/envconfig"
 )
 
 func Load(conf interface{}) error {
-	env := os.Getenv("GO_ENV")
-
-	// only load .env in dev mode
-	if env == "development" {
-		err := godotenv.Load()
-		if err != nil {
-			return fmt.Errorf("falied to .env: %v", err)
-		}
-	}
-
 	if err := envconfig.Init(conf); err != nil {
 		return fmt.Errorf("falied to load config: %v", err)
 	}
