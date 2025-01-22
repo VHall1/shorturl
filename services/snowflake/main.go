83be93e75d8514163bbd18d3bb5beac09ef51691
VHall1 <victorhallpsn@hotmail.com>
2025-01-22 14:23:01 +0000

    use router on snowflake service

--- services/snowflake/main.go
+++ services/snowflake/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/vhall1/shorturl/lib/bootstrap"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	httpServer, err := bootstrap.NewHttpServer()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := httpServer.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
