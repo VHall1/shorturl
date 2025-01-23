b55b14be60217ea4df5ba224b80080aeeae89541
VHall1 <victorhallpsn@hotmail.com>
2025-01-23 17:04:53 +0000

    add grpc server bootstrap

---
+++ lib/bootstrap/grpc.go
@@ -0,0 +1,68 @@
+package bootstrap
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/vhall1/shorturl/lib/config"
+	"google.golang.org/grpc"
+)
+
+type GrpcServer struct {
+	Server   *grpc.Server
+	listener *net.Listener
+	addr     string
+}
+
+type grpcServerConf struct {
+	GrpcAddr string `envconfig:"optional"`
+}
+
+func NewGrpcServer() (*GrpcServer, error) {
+	conf := &grpcServerConf{}
+	if err := config.Load(conf); err != nil {
+		return nil, err
+	}
+
+	addr := conf.GrpcAddr
+	if addr == "" {
+		fmt.Printf("[gRPC] no address set. Using :50051 fallback\n")
+		addr = ":50051"
+	}
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	s := grpc.NewServer()
+
+	return &GrpcServer{
+		Server:   s,
+		listener: &lis,
+		addr:     addr,
+	}, nil
+}
+
+func (s *GrpcServer) Start() error {
+	// listen for SIGINT and SIGTERM shutdown signals
+	go func() {
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+		<-sigs
+		fmt.Println("[gRPC] Shutdown signal received. Gracefully shutting down server")
+		s.Server.GracefulStop()
+	}()
+
+	ch := make(chan error)
+	go func() {
+		log.Printf("[gRPC] Server listening on %s\n", s.addr)
+		ch <- s.Server.Serve(*s.listener)
+	}()
+
+	err := <-ch
+	return err
+}
