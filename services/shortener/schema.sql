152e03b65052d90f35016bfb0c49bace7bb67e75
VHall1 <victorhallpsn@hotmail.com>
2025-01-24 15:25:18 +0000

    update sql schema

--- services/shortener/schema.sql
+++ services/shortener/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS "url" (
-  "id"       BIGINT NOT NULL,
-  "shortUrl" VARCHAR(11) NOT NULL,
-  "longUrl"  TEXT NOT NULL
+  "id"       BIGINT PRIMARY KEY,
+  "shortUrl" VARCHAR(11) NOT NULL UNIQUE,
+  "longUrl"  TEXT NOT NULL UNIQUE
 );
