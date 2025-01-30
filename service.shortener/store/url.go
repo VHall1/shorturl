7b9fa7c4182d29a818c2303631dda2e69656f90d
VHall1 <victorhallpsn@hotmail.com>
2025-01-30 15:46:50 +0000

    stub UrlStore

---
+++ service.shortener/store/url.go
@@ -0,0 +1,31 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/vhall1/shorturl/service.shortener/common"
+)
+
+type UrlStore struct {
+	db *sql.DB
+}
+
+func NewUrlStore(db *sql.DB) *UrlStore {
+	return &UrlStore{db: db}
+}
+
+func (s *UrlStore) Create(ctx context.Context, url *common.Url) error {
+	q := `
+		INSERT INTO "url" ("id", "shortUrl", "longUrl")
+		VALUES (:id, :shortUrl, :longUrl)
+	`
+
+	_, err := s.db.ExecContext(ctx, q,
+		sql.Named("id", url.Id),
+		sql.Named("shortUrl", url.ShortUrl),
+		sql.Named("longUrl", url.LongUrl),
+	)
+
+	return err
+}
