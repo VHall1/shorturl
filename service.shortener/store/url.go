f9114ce6925f2187f63dc0e6db5a5f69cf2acde1
VHall1 <victorhallpsn@hotmail.com>
2025-01-30 17:14:29 +0000

    implement UrlStore find methods

--- service.shortener/store/url.go
+++ service.shortener/store/url.go
@@ -3,6 +3,7 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	"github.com/vhall1/shorturl/service.shortener/common"
 )
@@ -24,4 +25,36 @@ func (s *UrlStore) Create(ctx context.Context, url *common.Url) error {
 	_, err := s.db.ExecContext(ctx, q, url.Id, url.ShortUrl, url.LongUrl)
 
 	return err
+}
+
+func (s *UrlStore) FindByLongUrl(ctx context.Context, longUrl string) (string, error) {
+	row := s.db.QueryRowContext(ctx, "SELECT * FROM url WHERE longUrl = ?", longUrl)
+
+	url, err := scanIntoUrl(row)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return "", err
+	}
+
+	return url.ShortUrl, nil
+}
+
+func (s *UrlStore) FindByShortUrl(ctx context.Context, shortUrl string) (string, error) {
+	row := s.db.QueryRowContext(ctx, "SELECT * FROM url WHERE shortUrl = ?", shortUrl)
+
+	url, err := scanIntoUrl(row)
+	if err != nil && !errors.Is(err, sql.ErrNoRows) {
+		return "", err
+	}
+
+	return url.LongUrl, nil
+}
+
+func scanIntoUrl(row *sql.Row) (*common.Url, error) {
+	url := new(common.Url)
+
+	if err := row.Scan(&url.Id, &url.LongUrl, &url.ShortUrl); err != nil {
+		return nil, err
+	}
+
+	return url, nil
 }
