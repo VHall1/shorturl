f77e88cc7e0e37297ef4ba7a8ce4e691b459f609
VHall1 <victorhallpsn@hotmail.com>
2025-01-30 16:04:27 +0000

    remove test /hello route

--- service.shortener/handler/get_hello.go
+++
@@ -1,9 +0,0 @@
-package handler
-
-import "net/http"
-
-func handleGetHello() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello"))
-	})
-}
--- service.shortener/handler/routes.go
+++ service.shortener/handler/routes.go
@@ -3,7 +3,5 @@ package handler
 import "net/http"
 
 func RegisterRoutes(router *http.ServeMux) {
-	router.Handle("GET /hello", handleGetHello())
-
 	router.Handle("POST /", handlePostShortenUrl())
 }
