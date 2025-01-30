1d470db34ea04fdc655b7a2a2e98ab1a002ae9cc
VHall1 <victorhallpsn@hotmail.com>
2025-01-30 13:47:16 +0000

    stub URL service (no logic)

--- service.shortener/domain/url.go
+++ service.shortener/domain/url.go
@@ -1,6 +1,19 @@
 package domain
 
+import "context"
+
 type URL struct {
 	ID                int64
 	ShortURL, LongURL string
+}
+
+type URLService struct{}
+
+func NewURLService() *URLService {
+	return &URLService{}
+}
+
+// TODO: Implement this
+func (s *URLService) ShortenURL(ctx context.Context, longURL string) (string, error) {
+	return "14q60P", nil
 }
