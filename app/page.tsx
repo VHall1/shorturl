7d91ee23302390d93c5d9464bd0044556f8370cf
vhall1 <victorhallpsn@hotmail.com>
2025-03-11 15:18:11 +0000

    Tweak shortener logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Form from "next/form";
 
 export default function Home() {
   return (
-    <Form action="/shortener">
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label htmlFor="email">URL</Label>
-        <Input name="query" placeholder="https://example.com" />
-      </div>
+    <Card className="max-w-md mx-auto">
+      <CardContent>
+        <Form action="/shorten">
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+            <Label htmlFor="url">URL</Label>
+            <Input id="url" name="url" placeholder="https://example.com" />
+          </div>
 
-      <Button type="submit" className="mt-4">Shorten URL</Button>
-    </Form>
+          <Button type="submit" className="mt-4 w-full lg:w-auto">
+            Shorten URL
+          </Button>
+        </Form>
+      </CardContent>
+    </Card>
   );
 }
--- app/shorten/page.tsx
+++ app/shorten/page.tsx
@@ -1,13 +1,24 @@
 import { tob62 } from "@/lib/base62";
 import { db } from "@/lib/db";
 import { snowflake } from "@/lib/snowflake";
+import { redirect } from "next/navigation";
 
 export default async function Page({
   searchParams,
 }: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const results = await getShortUrl((await searchParams).query);
+  const { url } = await searchParams;
+
+  if (!url) {
+    return redirect("/");
+  }
+
+  const results = await getShortUrl(
+    // searchParams can be composed into an array if the same key is defined multiple times in the request URL.
+    // Should be relatively safe to index the array here, as it takes at least 2 elements for `url` to come through as an array.
+    Array.isArray(url) ? url[0] : url
+  );
 
   return <p>{results}</p>;
 }
